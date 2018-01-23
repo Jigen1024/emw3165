5d74ef9f9db2cc223319f2abdd9f7b023bf29c54
Jigen <1431834893@qq.com>
2018-01-23 14:06:41 +0800

    添加 crc16 检验

---
+++ helloworld/util.c
@@ -0,0 +1,47 @@
+#include "mico.h"
+#include "util.h"
+
+void InvertUint8(uint8_t *dBuf, uint8_t *srcBuf) {
+	uint8_t tmp[4];
+	
+	tmp[0] = 0;
+	for(uint8_t i=0;i< 8;i++) {
+		if(srcBuf[0]& (1 << i)) {
+			tmp[0]|=1<<(7-i);
+		}
+	}
+	dBuf[0] = tmp[0];
+}
+
+void InvertUint16(uint16_t *dBuf, uint16_t *srcBuf) {
+    uint16_t tmp[4];
+    
+	tmp[0] = 0;
+    for(uint8_t i=0;i < 16;i++) {
+      if(srcBuf[0]& (1 << i))
+        tmp[0]|=1<<(15 - i);
+    }
+    dBuf[0] = tmp[0];
+}
+
+uint16_t CRC16_MODBUS(uint8_t *puchMsg, uint16_t usDataLen) {
+  uint16_t wCRCin = 0xFFFF;
+  uint16_t wCPoly = 0x8005;
+  uint8_t wChar = 0;
+
+  while ( usDataLen-- ) {
+        wChar = *(puchMsg++);
+        InvertUint8(&wChar,&wChar);
+        wCRCin ^= (wChar << 8);
+        for(uint8_t i = 0; i < 8; i++) {
+        	if(wCRCin & 0x8000) {
+        		wCRCin = (wCRCin << 1) ^ wCPoly;
+        	}
+        	else {
+        		wCRCin = wCRCin << 1;
+        	}
+        }
+  }
+  InvertUint16(&wCRCin, &wCRCin);
+  return wCRCin;
+}
---
+++ helloworld/util.h
@@ -0,0 +1,10 @@
+
+
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include "mico.h"
+
+uint16_t CRC16_MODBUS(uint8_t *puchMsg, uint16_t usDataLen);
+
+#endif
